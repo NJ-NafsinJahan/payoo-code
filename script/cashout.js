1f4d83c52a8e9e37707f6e59b7944d1c168f3122
NJ-NafsinJahan <nj.nafsinjahan@gmail.com>
2026-02-21 12:53:13 +0600

    add home and cashout section

---
+++ script/cashout.js
@@ -0,0 +1,9 @@
+document.getElementById("cashout-btn").addEventListener("click", function(){
+    // 1- get the agent number & validate(11 digit ar hote hobe)
+    // 2-get amount & validate & convert to number
+    // 3-get the current balance, validate & convert to number
+    // 4- calculate new balance
+    // 5-get the pin & verify
+    // 5-1-if true::: show an alert >> set new balance
+    // 5-1-if false::: show an error alert >> return
+});
